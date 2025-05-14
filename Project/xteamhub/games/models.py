ec63c0a9dce515f22228d1c3d5c08ba09daf8042
Samuel <145884210+samas-ai@users.noreply.github.com>
2025-05-14 12:29:16 -0300

    Delete Project/xteamhub directory
    Limpeza

--- Project/xteamhub/games/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Jogo(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    imagem = models.ImageField(upload_to='jogos/')
-
-    def __str__(self):
-        return self.nome
